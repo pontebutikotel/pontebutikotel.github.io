f8bb70ed6a8617ed1033ae9d3e6c6437da29580f
pontebutikotel <pontebutikotel@gmail.com>
2025-07-23 17:14:35 +0000

    ok

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-let lastScrollY = window.scrollY;
-
-window.addEventListener("scroll", () => {
-    const header = document.querySelector(".header");
-
-    if (window.scrollY > lastScrollY) {
-        // Sayfa aşağı kaydırılıyor
-        header.classList.add("hide");
-    } else {
-        // Sayfa yukarı kaydırılıyor
-        header.classList.remove("hide");
-    }
-
-    lastScrollY = window.scrollY;
-});
-
 document.addEventListener("DOMContentLoaded", function() {
     const menuIcon = document.getElementById("menu-icon");
     const navbar = document.querySelector(".navbar");
